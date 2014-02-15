2f2653070ef0c2a9e1c1079e7aa6798aa3424f79
xavierhd <xavierhd@hotmail.com>
2014-02-15 17:57:32 -0500

    Objet 0.2
    Ajout d'une class Vue, d'une class Controleur, découpage du programme et
    debug en masse

--- src/Daleks.py
+++ src/Daleks.py
@@ -1,9 +1,51 @@
 import random
 import os
+import sys
 import msvcrt
 import time
 
 #Declaration des objets
+class Vue:
+
+
+    #Fonction qui fait l'affichage de jeu dans la console
+    def afficher(self, jeu):
+    
+        #Nettoye l'ecran avant d'afficher le jeu de nouveau
+        os.system('cls')
+
+        #Affichage du terrain 
+        for i in range(0,jeu.surface_h+1):
+            for j in range(0, jeu.surface_l+1):
+
+                case_vide = True
+                
+                #Regarde dans la liste si il y a un element  a cette position
+                for n in range(0, jeu.nb_total_objets-1):
+                    
+                    #Si la case courante correspond a la case de la liste
+                    if(j == jeu.liste_objets[n].x and i == jeu.liste_objets[n].y):
+                        #Si c'est le docteur who
+                        if(n==0):
+                            print(chr(1),end='')
+                        #Si c'est un Dalek
+                        elif(isinstance(jeu.liste_objets[n], Dalek)):
+                            print(chr(177),end='')
+                        #Si c'est un tas de ferraille
+                        elif(isinstance(jeu.liste_objets[n], Ferraille)):
+                            print('#',end='')
+                        
+                        case_vide = False
+                   
+                if(case_vide == True):
+                    print('-',end='')
+                    
+            print('')   #end line
+
+        print('')   #end line
+        if(i == 0):
+            print('Points: '+jeu.liste_objets.points)
+
 
 class Jeu:
     def __init__(self):
@@ -11,25 +53,31 @@ class Jeu:
         self.level_complete = False     #Condition pour determiner si le niveau est complete
         self.points = 0                 #Variable qui contient le nombre de points accumules par le joueur
         self.vague = 0                  #Numero de la vague en cours
-        self.surface_l = 20             #Largeur de la surface de jeu
-        self.surface_h = 30             #Hauteur de la surface de jeu
         self.nb_total_dalek = 0         #Nombre total de Dalek pour la vague courrante
         self.nb_total_objets = 0        #Nombre d'elements dans la liste
         self.nb_dalek_restant = 0       #Nombre de Dalek restant sur la surface de jeu
         self.liste_objets = []          #Liste qui contiendra les daleks et le docteur
-        self.drWho = DrWho(random.randint(1, self.surface_l), random.randint(1, self.surface_h))  #Messieurs mesdames, je vous presente Dr. Who!  (Initialisation de la piece du joueur)
-
+        self.surface_l = 20             #Largeur de la surface de jeu
+        self.surface_h = 30             #Hauteur de la surface de jeu
 
     def setNextVague(self):
         self.nb_total_dalek += 5
         self.vague += 1
-        self.drWho.nb_zapper += 1
+        self.liste_objets[0].nb_zapper += 1
         self.creerListe()                                    #Creation de la liste pour la nouvelle vague
         self.nb_total_objets = len(self.liste_objets)   #Calcule le nombre d'objet dans la liste
 
-
-
-
+    def deplacerDalek(self,jeu):
+        for i in range(1, self.nb_total_objets-1): #regarde tout les objets de la liste
+                if(isinstance(self.liste_objets[i], Dalek)): #recherche d_objet Dalek
+                    self.liste_objets[i].deplacer()
+    
+    def denombreDalek(self):
+        compteur = 0
+        for i in range(1, self.nb_total_objets-1): #regarde tout les objets de la liste
+                if(isinstance(self.liste_objets[i], Dalek)): #recherche d_objet Dalek
+                    compteur +=1
+        return compteur
 
 
     #Fonction qui initialise la list contenant les objets(Personnages) pour le jeu (largeur de la surface, hauteur de la surface, nombre de daleks au total, liste qui contient les objets)
@@ -41,17 +89,21 @@ class Jeu:
         #Creation de variables temporaires pour les postions x et y de chaque piece
         x = 0   # 1 est la premiere case contrairement a 0 dans un tableau
         y = 0   # 1 est la premiere case contrairement a 0 dans un tableau
-        
-        self.nb_dalek_restant = self.nb_total_dalek
 
-        self.liste_objets[:] = [] #Vide l'ancienne liste d'objets
-     
-        #Creation d'une position au hasard pour le docteur
-        self.drWho.x = random.randint(1, self.surface_l)
-        self.drWho.y = random.randint(1, self.surface_h)
+        #Creation d'une position au hasard pour le docteur et inicialisation ci pas deja fait
+        try:
+            drWho = self.liste_objets[0] #Tentative de sauvegarder un docteur who deja existant
+        except:
+            drWho = DrWho(random.randint(1, self.surface_l),random.randint(1, self.surface_h)) #Creation d_un nouveau docteur who
+        else:
+            drWho.x = random.randint(1, self.surface_l)#Creation d'une position au hasard pour le docteur deja existant
+            drWho.y = random.randint(1, self.surface_h)
 
-        #ajout du dr who a la liste
-        self.liste_objets.append(self.drWho)
+        #Vide l'ancienne liste d'objets
+        self.liste_objets[:] = [] 
+ 
+        #ajout du dr who a la liste vide
+        self.liste_objets.append(drWho)
 
         #Creation de la liste en fonction du nombre total de dalek pour cette vague
         for i in range(0, self.nb_total_dalek):
@@ -61,7 +113,6 @@ class Jeu:
             #Tant que la position n'est pas une position valide par rapport a la postion du Docteur et n'est pas occupee
             while position_ok != True:
 
-                #Replace le position_xy a false au cas ou on devrait retester si position_ok n'est pas true
                 position_xy = False
 
                 #Regarde tout d'abord si la position n'est pas deja occupee par une autre piece
@@ -82,89 +133,65 @@ class Jeu:
 
 
             #Ajoute un Dalek valide a la liste
-            self.liste_objets.append(Dalek(x,y)
-
-
-
-
-
-
-    #Fonction qui fait l'affichage de jeu dans la console
-    def afficher(self):
-    
-        #Nettoye l'ecran avant d'afficher le jeu de nouveau
-        os.system('cls')
-
-        #Affichage du terrain 
-        for i in range(0,hauteur+1):
-            for j in range(0, largeur+1):
-
-                case_vide = True
-                
-                #Regarde dans la liste si il y a un element  a cette position
-                for n in range(0, nb_total_objets):
-                    
-                    #Si la case courante corespond a la case de la liste 
-                    if(j == self.liste_objets[n].x and i == self.liste_objets[n].y):
-                        #Si c'est un Dalek
-                        if(self.liste_objets[n].type_piece == 0):
-                            print(chr(177),end='')
-                        #Si c'est un tas de feraille
-                        if(self.liste_objets[n].type_piece == 1):
-                            print('#',end='')
-                        #Si c'est le docteur who
-                        if(self.liste_objets[n].type_piece == 2):
-                            print(chr(1),end='')
-
-                        case_vide = False
-                   
-                if(case_vide == True):
-                    print('-',end='')
-                    
-            print('')
-
-        print('')
-        print('Points: '+self.liste_objets.points)
+            self.liste_objets.append(Dalek(x,y,5))
 
 
 
     #Fonction gerant les collisions des daleks entre eux et les ferrailles
     def collision(self):
+
         liste_a_POPER = []      #liste de tous les objets a retirer de la liste
-        unChiffre = -1          #-1 est un moyen broche a foin de faire marcher cette fonction
-        for i in range(1, nb_total_objets):      #regarde tout les objets de la liste
+
+        for i in range(1, self.nb_total_objets-1):       #regarde tout les objets de la liste
+            if(isinstance(self.liste_objets[i], Dalek)):
+                for j in range(1, self.nb_total_objets-1):   #regarde tout les objets de la liste afin de comparer
+                    if(i != j): #les deux objets ne doivent pas etre le meme
+                        if(self.liste_objets[i].x == self.liste_objets[j].x and self.liste_objets[i].y == self.liste_objets[j].y): #Verifie si les deux pieces sont sur la meme case
+                            self.points += self.liste_objets[i].valeurPoint #ajout des points au total
+                            liste_a_POPER.append(i) #ajout a la liste de suppression
+
+        for i in range(1, len(liste_a_POPER)-1):
+            self.jeu.liste_objets.append( Ferraille(self.jeu.liste_objets[liste_a_POPER[i]].x, self.jeu.liste_objets[liste_a_POPER[i]].y) )
+            self.jeu.liste_objets.pop(liste_a_POPER[i])
+
+"""Criss de gros boute de code qui revient a faire ce qui est juste au dessus... (vive les dessins)
+        
+        for i in range(1, nb_total_objets-1):      #regarde tout les objets de la liste
             if(isinstance(jeu.liste_objets[i]),Dalek): #On veux seulement des dalek pour l_objet i
-                for j in range(1, nb_total_objets):#regarde tout les objets de la liste afin de comparer
+                for j in range(1, nb_total_objets-1):#regarde tout les objets de la liste afin de comparer
                     if(i != j): #les deux objets ne doivent pas etre le meme
                         if(self.liste_objets[i].x == self.liste_objets[j].x and self.liste_objets[i].y == self.liste_objets[j].y): #Verifie si les deux pieces sont sur la meme case
                             if(len(liste_a_POPER) > 0): #verifie s_il y a des element dans la liste de suppression
-                                unChiffre = 0           
                                 jOK = True
                                 iOK = True
-                            for k in range(unChiffre, len(liste_a_POPER)-1):  #-1 est un moyen broche a foin de faire marcher cette fonction (sinon il ne rentre pas une seul fois dans aucune de ces verification)
-                                if(j != liste_a_POPER[k]): #verifie que j n_est pas un objet a supprimer
-                                    jOK = False #Afin de savoir si j est a supprimer et vaux des points
-                                if(i != liste_a_POPER[k]):
-                                    iOK = False #Afin de savoir si i est a supprimer
+                                for k in range(0, len(liste_a_POPER)-1):  #-1 est un moyen broche a foin de faire marcher cette fonction (sinon il ne rentre pas une seul fois dans aucune de ces verification)
+                                    if(j != liste_a_POPER[k]):
+                                        jOK = False #Afin de savoir si j est a supprimer et vaux des points
+                                    if(i != liste_a_POPER[k]):
+                                        iOK = False #Afin de savoir si i est a supprimer et vaux des points
 
+ 
+                        #Si la piece est un tas de ferraille
+                        if(isinstance(jeu.liste_objets[j]),Ferraille and iOK == True):
+                            liste_a_POPER.append(i) #ajout a la liste de suppression
+                            jeu.points += jeu.liste_objets[i].valeurPoint #ajout des points au total
                             
-                                #Si la piece est un tas de ferraille
-                                if(isinstance(jeu.liste_objets[j]),Ferraille and iOK == True):
-                                    liste_a_POPER.append(i) #ajout a la liste de suppression
-                                    jeu.points += jeu.liste_objets[i].valeurPoint #ajout des points au total
-                                    
+            
+                        #Si la piece est un Dalek 
+                        elif(isinstance(jeu.liste_objets[j]),Dalek and jOK == True and iOK == True):
+                            liste_a_POPER.append(i) #ajout a la liste de suppression
+                            self.liste_objets.append(Ferraille(self.liste_objets[i].x, self.liste_objets[i].y))#ajout d_une ferraille
+                            jeu.points += jeu.liste_objets[i].valeurPoint    #ajout des points au total
                     
-                                #Si la piece est un Dalek 
-                                elif(isinstance(jeu.liste_objets[j]),Dalek and jOK == True and iOK == True):
-                                    liste_a_POPER.append(i) #ajout a la liste de suppression
-                                    self.liste_objets.append(Ferraille(self.liste_objets[i].x, self.liste_objets[i].y))#ajout d_une ferraille
-                                    jeu.points += jeu.liste_objets[i].valeurPoint    #ajout des points au total
-                            
-                                elif(jOK == False and iOK == True): #pas besoin de verifier le type d_objet, je te gage 10 piastre que c_est un dalek
-                                    liste_a_POPER.append(i) #ajout a la liste de suppression
-                                    jeu.points += jeu.liste_objets[i].valeurPoint    #ajout des points au total               
-                    
+                        elif(jOK == False and iOK == True): #pas besoin de verifier le type d_objet, je te gage 10 piastre que c_est un dalek
+                            liste_a_POPER.append(i) #ajout a la liste de suppression
+                            jeu.points += jeu.liste_objets[i].valeurPoint    #ajout des points au total
 
+        for i in range(1, len(liste_a_POPER)-1)
+            self.jeu.liste_objets.append( Ferraille(self.jeu.liste_objets[liste_a_POPER[i]].x, self.jeu.liste_objets[liste_a_POPER[i]].y) )
+            self.jeu.liste_objets.pop(liste_a_POPER[i])               
+            
+"""
 
 #Classe pour les objets qui seront dans la surface de jeu 
 class DrWho:
@@ -173,13 +200,13 @@ class DrWho:
         self.y = y; #Position en y sur la surface de jeu
         self.nb_zapper = 0 #Nombre de zapper dont a droit Dr Who
 
-    def is_he_dead(jeu):
+    def is_he_dead(self, jeu):
         for i in range(1,len(jeu.liste_objets-1)):
             if(self.x == jeu.liste_objets[i].x and self.y == jeu.liste_objets[i].y):
                 return True #veux dire qu_il est mort
 
      #Fonction qui gere le deplacement du joueur
-    def deplacer(jeu):
+    def deplacer(self, jeu):
 
         deplacement_valide = False
 
@@ -207,6 +234,11 @@ class DrWho:
                 v_x = 1
                 v_y = 1
 
+            #Pas de deplacement
+            if(key == b'5'):
+                v_x = 0
+                v_y = 0
+
             #Depplacement vers la droite
             if(key == b'6'):
                 v_x = 1
@@ -233,7 +265,7 @@ class DrWho:
                 v_y = 0 
 
             #Teleportation
-            if(key == b'5'):
+            if(key == b'/'):
                 print('')
                            
             #Zappeur
@@ -241,7 +273,7 @@ class DrWho:
                 print('')
             
             #Determine is le deplacement sera a l'interieur de la zone de jeu
-            if(jeu.liste_objets[0].x+v_x > largeur or jeu.liste_objets[0].y+v_y > hauteur or jeu.liste_objets[0].x+v_x < 0 or jeu.liste_objets[0].y+v_y < 0):
+            if(jeu.liste_objets[0].x+v_x > jeu.surface_l or jeu.liste_objets[0].y+v_y > jeu.surface_h or jeu.liste_objets[0].x+v_x < 0 or jeu.liste_objets[0].y+v_y < 0):
                 deplacement_valide = False
 
             #Determine si il y a une piece sur l'endroit ou le joueur veut se deplacer
@@ -249,7 +281,7 @@ class DrWho:
 
                 deplacement_valide = True
 
-                for i in range(1, nb_total_objets):
+                for i in range(1, jeu.nb_total_objets):
 
                     #Si i y a une piece a l'endroit ou le joueur veut se deplacer
                     if(jeu.liste_objets[0].x+v_x == jeu.liste_objets[i].x and jeu.liste_objets[0].y+v_y == jeu.liste_objets[i].y):
@@ -357,49 +389,66 @@ class _GetchWindows:
 
 
     
-#Game Loop ------------------------------------------------------------------------------------------------
 
-jeu = Jeu()
-
-level_complete = False
-
-while level_complete != True:
-
-    #Preparation de la vague, increment les dalek, zappeur, et autre goodies. Cree une liste d'objet contenant les dalek et le docteur
-    jeu.setNextVague()
-    #Affichage de la surface de jeu 
-    jeu.afficher()
-    print(liste_info[1])
+class Controleur:
     
-    #Deplacement du joueur
-    jeu.liste[0].deplacer(jeu)
-    print(liste_objets[0].x,liste_objets[0].y)
-    
-    #Affichage de la surface de jeu 
-    jeu.afficher()
-    time.sleep(0.5)
-    
-    #Deplacement automatique des Daleks
-    for i in range(1, nb_total_objets): #regarde tout les objets de la liste
-        if(isinstance(jeu.liste[i], Dalek)): #recherche d_objet Dalek
-            jeu.liste[i].deplacer(jeu)
 
-    #Verifie les collisions
-    jeu.collision()
-
-    #Verifie si le joueur a ete capturer
-    jeu.liste[0].is_he_dead()
-     
-    #Verifie si il ne reste plus de daleks
+    def __init__(self):
+        self.jeu = Jeu()
+        self.vue = Vue()
     
+
+    def main(self):
+        #call du menu
+        retourMenu = 0 #fonction menu ICI
+        if (retourMenu == 0):
+            self.gameLOOP()
+        elif (retourMenu == 1):
+            pass #do something
+        elif (retourMenu == 2):
+            pass #do something
+        elif (retourMenu == 3):
+            pass #do something
+
+    def gameLOOP(self):
+
+        #
+        self.jeu.creerListe()
+
+        #Preparation de la vague, increment les dalek, zappeur, et autre goodies. Cree une liste d'objet contenant les dalek et le docteur
+        self.jeu.setNextVague()
         
+        while self.jeu.denombreDalek() != 0 or self.jeu.liste[0].is_he_dead():#Verifie si il ne reste plus de daleks ou #Verifie si le joueur a ete capturer
+
+            #Affichage de la surface de jeu 
+            self.vue.afficher(self.jeu)
             
-        
-#----------------------------------------------------------------------------------------------------------
+            #Deplacement du joueur
+            self.jeu.liste_objets[0].deplacer(self.jeu)
+            
+            #Affichage de la surface de jeu 
+            self.vue.afficher(self.jeu)
+            time.sleep(0.5)
+            
+            #Deplacement automatique des Daleks
+            self.jeu.deplacerDalek(self.jeu)
 
-os.system('cls')
-print('GAME OVER !!!')
-print('')
-print('Vague : '+str(vague))
-print('Points : '+str(liste_info[0]))
-msvcrt.getch()
+            #Verifie les collisions
+            self.jeu.collision()
+
+             
+
+    def endGame(self):
+        os.system('cls')
+        print('GAME OVER !!!')
+        print('')
+        print('Vague : '+str(self.jeu.vague))
+        print('Points : '+str(self.jeu.Points))
+        msvcrt.getch()
+
+        
+
+
+if __name__ == "__main__":
+    c = Controleur()
+    sys.exit(c.main())
